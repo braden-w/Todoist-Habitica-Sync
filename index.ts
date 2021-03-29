a1c508c72d1b2648f1a1ae93411849441a614230
Braden <bmw02002turbo@gmail.com>
2021-03-28 22:02:59 -0700

    Import axios

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const axios = require("axios")
 
 // Tell express to use body-parser's JSON parsing
 app.use(express.json())
