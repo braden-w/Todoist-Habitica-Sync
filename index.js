87748a2cb52e344b9cc2083f87a84cda9abb9c24
Braden <bmw02002turbo@gmail.com>
2021-03-28 20:41:31 -0700

    Add hook path

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ app.use(bodyParser.json())
 
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.post("/hook", (req, res) => {
+  console.log(req.body) // Call your action on the request here
+  res.status(200).end() // Responding is important
+})
