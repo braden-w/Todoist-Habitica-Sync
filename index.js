86f65227a79296641a53edce1b9d4f0bd30fadee
Braden <bmw02002turbo@gmail.com>
2021-03-28 20:53:46 -0700

    Clean up node setup

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Require express and body-parser
 const express = require("express")
-const bodyParser = require("body-parser")
-
-// Initialize express and define a port
 const app = express()
 const PORT = 3000
 
