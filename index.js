e5aea9f43fb2f470030fb3caaeca149cffb057e5
Braden <bmw02002turbo@gmail.com>
2021-03-28 20:53:58 -0700

    Get for root

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const PORT = 3000
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
+app.get("/", (req, res) => {
+  res.send("Hello!")
+})
+
 app.post("/hook", (req, res) => {
   console.log(req.body) // Call your action on the request here
   res.status(200).end() // Responding is important
