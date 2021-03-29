750f497b53e78bce471ced43ea26aaa98f4e541a
Braden <bmw02002turbo@gmail.com>
2021-03-28 22:49:04 -0700

    Switch for apiURL

--- index.ts
+++ index.ts
@@ -17,16 +17,22 @@ app.get("/", (req, res) => {
 app.post("/hook", async (req, res) => {
   const { event_data, event_name } = req.body
   console.log(event_data, event_name)
+  let apiURL = ""
   switch (event_name) {
     case "item:added":
+      apiURL = "https://habitica.com/api/v3/tasks/user"
       break
     case "item:updated":
+      apiURL = `https://habitica.com/api/v3/tasks/${event_data.id}`
       break
     case "item:completed":
+      apiURL = `habitica.com/api/v3/tasks/${event_data.id}/score/up`
       break
     case "item:uncompleted":
+      apiURL = `habitica.com/api/v3/tasks/${event_data.id}/score/down`
       break
     case "item:deleted":
+      apiURL = `habitica.com/api/v3/tasks/${event_data.id}`
       break
   }
   const payload = {
